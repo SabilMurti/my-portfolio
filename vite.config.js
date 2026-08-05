85366c07c29004a4aa6b8443d2b9f3a618b9e7d9
SabilMurti <isabilmurti@gmail.com>
2026-08-05 18:54:47 +0700

    chore: update base URL for GitHub Pages to /my-portfolio/

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Base path matching the new repository name for GitHub Pages
-  base: '/murtix-portfolio/', 
+  base: '/my-portfolio/', 
 });
